85a2932adbc9a132910eb5aa265b8091c401ca88
Lautaro Gutierrez Lardit <lau.gutlar55@gmail.com>
2025-10-13 15:41:41 -0300

    Cambios

--- tateti.js
+++ tateti.js
@@ -11,14 +11,33 @@ function togglePlayer(board) {
 function checkWinner(board) {
     // Todas las combinaciones posibles para ganar
     const winnerCombinations = [
-        [0, 1, 2], // fila superior
-        [3, 4, 5], // fila del medio
-        [6, 7, 8], // fila inferior
-        [0, 3, 6], // columna izquierda
-        [1, 4, 7], // columna central
-        [2, 5, 8], // columna derecha
-        [0, 4, 8], // diagonal principal
-        [2, 4, 6]  // diagonal inversa
+        [0, 6, 12, 18]
+        [1, 7, 13, 19]
+        [5, 11, 17, 23]
+        [6, 12, 18, 24]
+        [3, 7, 11, 15]
+        [4, 8, 12, 16]
+        [8, 12, 16, 20]
+        [9, 13, 17, 21]
+        [0, 1, 2, 3]
+        [5, 6, 7, 8]
+        [6, 7, 8, 9]
+        [10, 11, 12, 13]
+        [11, 12, 13, 14]
+        [15, 16, 17, 18]
+        [16, 17, 18, 19]
+        [20, 21, 22, 23]
+        [21, 22, 23, 24]
+        [0, 5, 10, 15]
+        [5, 10, 15, 20]
+        [1, 6, 11, 16]
+        [6, 11, 16, 21]
+        [2, 7, 12, 17]
+        [7, 12, 17, 22]
+        [3, 8, 13, 18]
+        [8, 13, 18, 23]
+        [4, 9, 14, 19]
+        [9, 14, 19, 24]
     ];
 
     // Revisar cada combinación
@@ -33,11 +52,11 @@ function checkWinner(board) {
 }
 
 function findWinnerMove(board, player) {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 25; i++) {
         if (board [i] === 0 ) {
             const TemporaryBoard=[...board];
-             temporaryBoard[i] = player;
-            if (verificador (temporaryBoard) === player) {
+             TemporaryBoard[i] = player;
+            if (verificador (TemporaryBoard) === player) {
                 return i;
             }
         }
